774401c8552002f1c8ca2c8ba057e6cddb01593b
JoseG <jgomez0793@gmail.com>
2020-12-02 23:50:28 +0100

    cfcb7d02aef98e4d0aad8d3b3c276d99d08bf7b9 (tag: v1.3)

--- Resultados_Matlab/MenuResultados.m
+++ Resultados_Matlab/MenuResultados.m
@@ -42,16 +42,152 @@ switch choice
         grid on;
         
     case 3
-        cd('..\Modelo\SpaceFrame\Reactions');
-        x1 = load('node1reac.out'); 
-        x4 = load('node4reac.out');
-        x7 = load('node7reac.out'); 
-        x10 = load('node10reac.out');
-
-        x1 = x1(:,2);
-        x4 = x4(:,2);
-        x7 = x7(:,2);
-        x10 = x10(:,2);
+       cd('..\Modelo\SpaceFrame\Reactions');
+       %Depuracion en el nodo 1
+       fichero = fopen('node1reac.out', 'r');
+       i = 1;
+       linea = 1;
+       tline1 = fgetl(fichero);
+       var1 = length(split(string(tline1)));
+       fichero = fopen('node1reac.out','r');
+       
+        while (~feof(fichero))
+            tline = fgetl(fichero);
+            tline = string(tline);
+            tline = split(tline);
+            var = length(tline);
+            try
+                if (var1 == var)
+                    for j = 1:var
+                        datos(i,j) = str2num(tline(j));
+                    end
+                elseif (var == 0)
+                    display(linea);
+                    i = i-1;
+                elseif (var < var1)
+                    display(linea)
+                    i = i-1;
+                end
+                end
+            i = i+1;
+            linea = linea + 1;
+        end
+        tamanio = size(datos);
+        for k = 2: tamanio(1)
+            if datos(k,1) < datos(k-1,1)
+                display(k);
+                datos(k:end) = [];
+            end
+        end
+        x1 = datos(:,2);
+        
+        %Nodo 4 reacciones
+        fichero = fopen('node4reac.out', 'r');
+        i = 1;
+        linea = 1;
+        tline1 = fgetl(fichero);
+        var1 = length(split(string(tline1)));
+        fichero = fopen('node4reac.out','r');
+       
+        while (~feof(fichero))
+            tline = fgetl(fichero);
+            tline = string(tline);
+            tline = split(tline);
+            var = length(tline);
+            if (var1 == var)
+                for j = 1:var
+                    datos(i,j) = str2num(tline(j));
+                end
+            elseif (var == 0)
+                display(linea);
+                i = i-1;
+            elseif (var < var1)
+                display(linea)
+                i = i-1;
+            end
+            i = i+1;
+            linea = linea + 1;
+        end
+        tamanio = size(datos);
+        for k = 2: tamanio(1)
+            if datos(k,1) < datos(k-1,1)
+                display(k);
+                datos(k:end) = [];
+            end
+        end
+        x4 = datos(:,2);
+        
+        %Depuracion reaccion en el nodo 7
+        fichero = fopen('node7reac.out', 'r');
+        i = 1;
+        linea = 1;
+        tline1 = fgetl(fichero);
+        var1 = length(split(string(tline1)));
+        fichero = fopen('node7reac.out','r');
+       
+        while (~feof(fichero))
+            tline = fgetl(fichero);
+            tline = string(tline);
+            tline = split(tline);
+            var = length(tline);
+            if (var1 == var)
+                for j = 1:var
+                    datos(i,j) = str2num(tline(j));
+                end
+            elseif (var == 0)
+                display(linea);
+                i = i-1;
+            elseif (var < var1)
+                display(linea)
+                i = i-1;
+            end
+            i = i+1;
+            linea = linea + 1;
+        end
+        tamanio = size(datos);
+        for k = 2: tamanio(1)
+            if datos(k,1) < datos(k-1,1)
+                display(k);
+                datos(k:end) = [];
+            end
+        end
+        x7 = datos(:,2);
+        
+        %Nodo 10
+        fichero = fopen('node10reac.out', 'r');
+        i = 1;
+        linea = 1;
+        tline1 = fgetl(fichero);
+        var1 = length(split(string(tline1)));
+        fichero = fopen('node10reac.out','r');
+       
+        while (~feof(fichero))
+            tline = fgetl(fichero);
+            tline = string(tline);
+            tline = split(tline);
+            var = length(tline);
+            if (var1 == var)
+                for j = 1:var
+                    datos(i,j) = str2num(tline(j));
+                end
+            elseif (var == 0)
+                display(linea);
+                i = i-1;
+            elseif (var < var1)
+                display(linea)
+                i = i-1;
+            end
+            i = i+1;
+            linea = linea + 1;
+        end
+        tamanio = size(datos);
+        for k = 2: tamanio(1)
+            if datos(k,1) < datos(k-1,1)
+                display(k);
+                datos(k:end) = [];
+            end
+        end
+        x10 = datos(:,2);
 
         Cortante = x1 + x4 + x7 + x10;
         Cortante = -Cortante;
